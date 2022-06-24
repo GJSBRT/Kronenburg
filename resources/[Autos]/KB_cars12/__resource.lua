61e0b461ce08c407ed284d8ab3ba0075b4230503
GJSBRT <gijsbert@dutchis.net>
2022-06-24 23:58:11 +0200

    Autos

---
+++ resources/[Autos]/KB_cars12/__resource.lua
@@ -0,0 +1,74 @@
+fx_version 'cerulean'
+games { 'gta5' }
+files {
+	'data/**/vehicles.meta',
+	'data/**/carvariations.meta',
+	'data/**/carcols.meta',
+	'data/**/handling.meta',
+	'data/**/dlctext.meta',
+	'data/**/vehiclelayouts.meta',
+	'data/**/shop_vehicle.meta',
+	'data/**/carcontentunlocks.meta',
+
+	'els/anwbcaddy.xml',
+	'els/anwbhyundai.xml',
+	'els/anwbt6.xml',
+	'els/anwbvito.xml',
+	'els/flatbed3.xml',
+        'els/anwbmoetroe.xml',
+	'els/ambusprinter',
+	'els/ambutouran2016',
+	'els/kmar6.xml',
+	'els/ov3.xml',
+	'els/ov28.xml',
+	'els/pamarok.xml',
+	'els/polgolf7oov.xml',
+	'els/upolitiea4.xml',
+	'els/polvito2016.xml',
+	'els/poltransporter6.xml',
+	'els/poltransporter5.xml',
+	'els/poltouran2016.xml',
+	'els/poltouran2009.xml',
+	'els/polt6un.xml',
+	'els/polrs6.xml',
+	'els/polmotor3.xml',
+	'els/polmotor2.xml',
+	'els/polmotor.xml',
+	'els/polmerc.xml',
+	'els/poljeep.xml',
+	'els/PolitieTouranK9.xml',
+	'els/PolitieAtBus.xml',
+	'els/polgolf7oov.xml',
+	'els/polbus3.xml',
+	'els/polbmwx5v2.xml',
+	'els/polaudi.xml',
+	'els/pamarok.xml',
+	'els/ov28.xml',
+	'els/ov3.xml',
+	'els/kmar6.xml',
+	'els/ambutouran2016.xml',
+	'els/ambusprinter.xml',
+	'els/vwtouareg.xml',
+        'els/polmotorun.xml',
+        'els/kmar3.xml',
+
+
+	
+
+	
+
+
+
+}
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'VEHICLE_SHOP_FILE' 'data/**/shop_vehicle.meta'
+data_file 'CARCONTENTUNLOCKS_FILE' 'data/**/carcontentunlocks.meta'
+
+is_els 'true'
+
+
---
+++ resources/[Autos]/KB_cars9/data/anwbtouran/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Bikes'
+
+version '1.3.0'
+
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    'vehiclelayouts.meta',
+    'anwbtouran.xml',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+
+is_els 'true'
