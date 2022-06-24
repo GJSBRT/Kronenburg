4761442e9f93f41a02e232a46b75edb60181da4f
GJSBRT <gijsbert@dutchis.net>
2022-06-25 00:45:09 +0200

    Autos

---
+++ resources/[Autos]/KB_cars9/vehicle_names.lua
@@ -0,0 +1,24 @@
+AddTextEntryByHash(GetHashKey("politiebklasse"),   "Mercedes B klasse")                  --225 km/h
+AddTextEntryByHash(GetHashKey("PolitieVito"),   "Mercedes Vito")                      --215 km/h
+AddTextEntryByHash(GetHashKey("PolitieT6"),   "Volkswagen T6")                      --205 km/h
+--AddTextEntryByHash(GetHashKey("pol6"),   "Volkswagen T6 Biketeam")             --220 km/h
+--AddTextEntryByHash(GetHashKey("pol7"),   "Volkswagen Amarok")                  --230 km/h
+--AddTextEntryByHash(GetHashKey("pol8"),   "Hyundai Kona")                       --N.V.T   
+AddTextEntryByHash(GetHashKey("PolitieTouran"),   "Volkswagen Touran")    --220 km/h
+--ddTextEntryByHash(GetHashKey("pol10"),  "Volkswagen Touareg")                 --230 km/h
+--AddTextEntryByHash(GetHashKey("pol11"),  "Volkswagen Passaat")                 --265 km/h
+AddTextEntryByHash(GetHashKey("PolitieV70"),  "Volvo V70")                          --290 km/h
+--AddTextEntryByHash(GetHashKey("pol13"),  "Audi A6 Siv")                        --320 km/h
+--AddTextEntryByHash(GetHashKey("pol14"),  "Yahama Lichte Moter")                --260 km/h
+--AddTextEntryByHash(GetHashKey("pol15"),  "Bmw Zware motor")                    --280 km/h
+--AddTextEntryByHash(GetHashKey("pol16"),  "Bmw K1600 Gt")                       --330 km/h
+--AddTextEntryByHash(GetHashKey("pol17"),  "Volkswagen Polo 2009 Unmarked")      --214 km/h
+--AddTextEntryByHash(GetHashKey("pol18"),  "Bmw 540I Unmarked")                  --255 km/h
+AddTextEntryByHash(GetHashKey("polrs6"),  "Audi Rs6 Unmarked")                  --270 km/h
+--AddTextEntryByHash(GetHashKey("pol20"),  "Volvo V70 Unmarked")                 --270 km/h
+--AddTextEntryByHash(GetHashKey("pol21"),  "Skoda Sedan Unmarked")               --230 km/h
+--AddTextEntryByHash(GetHashKey("pol22"),  "Mercedes Me-Flexbus")                --190 km/h
+AddTextEntryByHash(GetHashKey("polbus2"),  "Volkswagen Crafter Voa")             --190 km/h
+--AddTextEntryByHash(GetHashKey("pol24"),  "Volkswagen T6 Voa")                  --190 km/h
+AddTextEntryByHash(GetHashKey("PolitieXC90"),  "Volvo XC90")                    --230 km/h
+--AddTextEntryByHash(GetHashKey("Politieboot"), "Politie Boot")                  --70 km/h
