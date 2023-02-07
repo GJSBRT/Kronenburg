0b8c0b893da33e7a59009ab083dbe8bc26bfd76c
Gijsbert <gijsbert@dutchis.net>
2023-02-07 20:18:02 +0100

    Kleding

---
+++ resources/[EUP]/[daily-clothing]/dlcvrouw/fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_f_freemode_01_mp_f_dlcvrouw.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_dlcvrouw.meta'
+client_script "@watermeloen/acloader.lua"
